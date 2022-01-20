9f08d721b163ae82d8a82d97dae23b6c2c63f15d
widoto <minhaekim44@gmail.com>
2022-01-20 10:27:24 +0900

    test_minhae2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var button1: Button//이것도 테스트용 나중에 지우기기
+    //이것도 테스트용 나중에 지우기기
 
    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
