daaab39ed52db9d921722ade7e573b5392b12bec
han-seo <68280465+han-seo@users.noreply.github.com>
2022-01-21 15:27:14 +0900

    //카텍고리 및 버튼 구현

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var Button : Button
+
+
 
     //이것도 테스트용 나중에 지우기기
 
@@ -12,5 +19,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //잘되는지 테스트
+
+       Button =  findViewById<Button>(R.id.button)
+
+       Button.setOnClickListener{
+           val intent= Intent(this, category::class.java)
+           startActivity(intent)
+       }
+
+
     }
+
 }
