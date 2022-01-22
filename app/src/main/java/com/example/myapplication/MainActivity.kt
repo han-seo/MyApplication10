4a18a92e82febf53e359dbc17727a198a030fadc
han-seo <68280465+han-seo@users.noreply.github.com>
2022-01-22 21:10:25 +0900

    카테고리 별 속성 구현

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-    //이것도 테스트용 나중에 지우기기
+
+    //버튼 클릭 이벤트 리스너 구현
+    lateinit var but : Button
 
    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //잘되는지 테스트
+        but = findViewById(R.id.but)
+       but.setOnClickListener{
+           var intent = Intent(this,category::class.java)
+           startActivity(intent)
+       }
     }
 }
--- app/src/main/java/com/example/myapplication/studio.kt
+++ app/src/main/java/com/example/myapplication/studio.kt
@@ -1,11 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class studio : AppCompatActivity() {
+    lateinit var btn_Back : Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_studio)
+
+        btn_Back = findViewById(R.id.btn_Back)
+        btn_Back.setOnClickListener{
+            var intent = Intent(this,category::class.java)
+            startActivity(intent)
+        }
     }
+
 }
