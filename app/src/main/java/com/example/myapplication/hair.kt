f9cdd87067412b8fe698603103275adb6cf92f16
han-seo <68280465+han-seo@users.noreply.github.com>
2022-01-22 22:05:07 +0900

    카테고리 디자인 및 뒤로가기

--- app/src/main/java/com/example/myapplication/hair.kt
+++ app/src/main/java/com/example/myapplication/hair.kt
@@ -1,11 +1,21 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class hair : AppCompatActivity() {
+    lateinit var btn_Back2 : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hair)
+
+        btn_Back2 = findViewById(R.id.btn_Back2)
+        btn_Back2.setOnClickListener{
+            var intent = Intent(this,category::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/photo.kt
+++ app/src/main/java/com/example/myapplication/photo.kt
@@ -1,11 +1,22 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class photo : AppCompatActivity() {
+
+    lateinit var btn_Back3:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo)
+
+        btn_Back3 = findViewById(R.id.btn_Back3)
+        btn_Back3.setOnClickListener{
+            var intent = Intent(this,category::class.java)
+            startActivity(intent)
+        }
     }
 }
