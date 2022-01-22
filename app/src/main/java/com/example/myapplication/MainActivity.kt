8dd14e384e0054fbe8a47e4f7e5cb15ab8b61096
trishasu <68217355+trishasu@users.noreply.github.com>
2022-01-22 16:51:52 +0900

    회원가입,로그인 화면 연결

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    //이것도 테스트용 나중에 지우기기
-
    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //잘되는지 테스트
+       Btn_shift.setOnClickListener{
+           val intent = Intent(this, Login::class.java)
+           startActivity(intent) }
+
     }
 }
