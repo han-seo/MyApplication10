98fc5c0913762f9c31d75d4564044842c4be2736
trishasu <68217355+trishasu@users.noreply.github.com>
2022-01-24 10:15:10 +0900

    //로그인,회원가입 오류 수정

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,28 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     //테스트용 버튼
     lateinit var Btn_Go : Button
-    lateinit var Btn_shift : Button
+  //  lateinit var Btn_shift : Button
 
    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //잘되는지 테스트-->안됨
+        //잘되는지 테스트-->됨
        Btn_Go = findViewById(R.id.Btn_Go)
        Btn_Go.setOnClickListener{
            var intent = Intent(this, category::class.java)
            startActivity(intent)
        }
 
-/*       Btn_shift = findViewById(R.id.Btn_Go)
-       Btn_shift.setOnClickListener{
-           var intent = Intent(this, Login::class.java)
+     Btn_shift.setOnClickListener{
+           val intent = Intent(this, Login::class.java)
            startActivity(intent)
-       }*/
+       }
     }
 }
