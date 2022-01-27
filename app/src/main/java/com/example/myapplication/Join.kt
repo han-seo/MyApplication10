0659c3cc03c0f8aa13598ac6f90c3d2e7e37a8d7
trishasu <68217355+trishasu@users.noreply.github.com>
2022-01-27 14:15:00 +0900

    //개인정보 데이터 저장 (직업 빼고)

--- app/src/main/java/com/example/myapplication/Join.kt
+++ app/src/main/java/com/example/myapplication/Join.kt
@@ -2,12 +2,13 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 class Join : AppCompatActivity() {
 
     lateinit var Button5 : Button
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +23,27 @@ class Join : AppCompatActivity() {
         }
 
     }
+
+    fun printMessage (view: View) {
+        var edit_id: EditText = findViewById(R.id.edit_name)
+        var edit_name: EditText = findViewById(R.id.edit_name)
+        var edit_sns1: EditText = findViewById(R.id.edit_name)
+
+        var edit_id_value: String? = edit_id.text.toString()
+        var edit_name_value: String? = edit_name.text.toString()
+        var edit_sns1_value: String? = edit_sns1.text.toString()
+
+        var intent_for_print: Intent = Intent(this, ConfirmMyInfo::class.java)
+
+        intent_for_print.putExtra("key1", edit_id_value)
+        intent_for_print.putExtra("key1", edit_name_value)
+        intent_for_print.putExtra("key1", edit_sns1_value)
+
+        startActivity(intent_for_print)
+
+
+    }
+
 
 }
 
