068c17e5de2f415a963776332ac23b4a403744ea
trishasu <68217355+trishasu@users.noreply.github.com>
2022-01-28 01:27:16 +0900

    //하단바까지

--- app/src/main/java/com/example/myapplication/Join.kt
+++ app/src/main/java/com/example/myapplication/Join.kt
@@ -3,12 +3,16 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_join.*
+
 class Join : AppCompatActivity() {
 
-    lateinit var Button5 : Button
+    lateinit var Button5: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,14 +21,50 @@ class Join : AppCompatActivity() {
         Button5 = findViewById<Button>(R.id.btn_confirm)
 
         Button5.setOnClickListener {
-          val intent = Intent(this, Login::class.java )
+            val intent = Intent(this, Login::class.java)
 
             startActivity(intent)
         }
 
+        val items3 = resources.getStringArray(R.array.pick_array)
+        val myAdapter3 = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items3)
+
+        spinner3.adapter = myAdapter3
+        spinner3.prompt = "카테고리를 선택해주세요."
+        spinner3.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+            ) {
+
+                //아이템이 클릭 되면 맨 위부터 position 0번부터 순서대로 동작하게 됩니다.
+                when (position) {
+                    0 -> {
+
+                    }
+                    1 -> {
+
+                    }
+                    //...
+                    else -> {
+
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+
+            }
+
+        }
+
+//spinner.setSelection(2)
+
     }
 
-    fun printMessage (view: View) {
+    fun printMessage(view: View) {
         var edit_id: EditText = findViewById(R.id.edit_name)
         var edit_name: EditText = findViewById(R.id.edit_name)
         var edit_sns1: EditText = findViewById(R.id.edit_name)
@@ -44,6 +84,4 @@ class Join : AppCompatActivity() {
 
     }
 
-
 }
-
---
+++ app/src/main/java/com/example/myapplication/bar.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class bar :AppCompatActivity() {
+
+    private val fl: FrameLayout by lazy {
+        findViewById(R.id.fl_)
+    }
+    private val bn: BottomNavigationView by lazy {
+        findViewById(R.id.bn_)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().add(fl.id, Fragment() ).commit()
+        bn.setOnNavigationItemSelectedListener {
+            replaceFragment(
+               when (it.itemId)
+                {
+                    R.id.menu_home -> Home()
+                    //  R.id.menu_love -> 찜하기()
+                    R.id.menu_category -> Home()
+                    R.id.menu_mypage -> MyPage()
+                    else -> newUpload()
+                }
+            )
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: AppCompatActivity) {
+        supportFragmentManager.beginTransaction().replace(fl.id, Fragment() ).commit()
+    }
+}
+
