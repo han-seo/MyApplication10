31ffbaf6d1df390efce3d587b5139e555024b449
han-seo <68280465+han-seo@users.noreply.github.com>
2022-01-28 12:14:31 +0900

    //예약페이지

--- app/src/main/java/com/example/myapplication/Reserve.kt
+++ app/src/main/java/com/example/myapplication/Reserve.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_reserve.*
 import java.util.*
@@ -15,13 +16,26 @@ class Reserve : AppCompatActivity() {
         setContentView(R.layout.activity_reserve)
 
 
+        Btn_9=findViewById(R.id.Btn_9)
+
         calendarView.setOnDateChangeListener { calenderView, year, month, day ->
+
+            Reserve_tv.visibility= View.VISIBLE //  선택시 보이도록
+            linearLayout.visibility=View.VISIBLE
+            linearLayout2.visibility=View.VISIBLE
+
+            Btn_9.setOnClickListener{
+                Btn_reserve.visibility=View.VISIBLE
+            }
         }
         val calendar = Calendar.getInstance()
         println(calendar[Calendar.YEAR])
         println(calendar[Calendar.MONTH])
         println(calendar[Calendar.DAY_OF_MONTH])
+
+
+
     }
 
-
 }
+
